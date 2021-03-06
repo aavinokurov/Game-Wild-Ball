8d1a7befc004d9bfa74d8d29403388c4b5d5bf54
Александр Винокуров <aavinokurov@outlook.com>
2021-03-06 14:34:19 +0300

    Add: Main menu & Levels from last project

---
+++ Game Wild Ball/Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject levelPanel;
+    private GameObject currentPanel;
+
+    private void Start()
+    {
+        currentPanel = menuPanel;
+        currentPanel.SetActive(true);
+    }
+
+    public void OpenMenuPanel()
+    {
+        OpenPanel(menuPanel);
+    }
+
+    public void OpenLevelPanel()
+    {
+        OpenPanel(levelPanel);
+    }
+
+    private void OpenPanel(GameObject namePanel)
+    {
+        if (currentPanel != null)
+        {
+            currentPanel.SetActive(false);
+            currentPanel = namePanel;
+            currentPanel.SetActive(true);
+        }
+    }
+}
