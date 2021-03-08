e85d8e5233b13ab7ec274befa3cb9d009975e19f
aavinokurov <aavinokurov@outlook.com>
2021-03-08 18:40:18 +0300

    Add: Island 2

---
+++ Game Wild Ball/Assets/Scripts/OpenBridge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBridge : MonoBehaviour
+{
+    [SerializeField] private Animator bridgeAnim;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if(player.CompareTag("Player"))
+        {
+            bridgeAnim.SetBool("OpenTrigger", true);
+        }
+    }
+}
