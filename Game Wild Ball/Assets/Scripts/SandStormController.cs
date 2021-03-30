edf69374cd58d1ac3f3fcada0061b6650e009d1b
AlexandrVinokurov <aavinokurov@outlook.com>
2021-03-30 20:51:32 +0300

    Continued 3 level
    Add: materials, textures, sand storm

---
+++ Game Wild Ball/Assets/Scripts/SandStormController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandStormController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody playerRigidBody;
+    [SerializeField] private ParticleSystem sandStormParticle;
+    private Vector3 sandForce = new Vector3(10, 0, 0);
+    private System.Random rnd = new System.Random();
+    private float time;
+
+    private void Start()
+    {
+        time = rnd.Next(3, 8);
+        StartCoroutine(sandStromTimer());
+    }
+
+    private IEnumerator sandStromTimer()
+    {
+        yield return new WaitForSeconds(time);
+
+        playerRigidBody.AddForce(sandForce);
+        sandStormParticle.Play();
+        time = rnd.Next(3, 8);
+        StartCoroutine(sandStromTimer());
+    }    
+}
